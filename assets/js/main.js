5b1ac9716ed6aab5185ac95ff948e8e28871ad8d
Ninni-cfm <k.nienhaus@cfm-net.de>
2021-07-21 01:02:02 +0200

    Small changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,30 @@
+//*****************************************************************
+// define vars for the input elements (easy access)
+let txt = document.getElementById("color-text");
 let btn = document.getElementById("submit");
 
-function changeBodyColor() {
-    console.log("changeBodyColor()");
 
-    btn.style.boxShadow = "0 0 15px 5px blue";
+function buttonClicked() {
 
-    let bgColor = document.getElementById("color-value").value;
-    console.log(bgColor);
+    // draw a blue border around the button...
+    btn.classList.add("blue-border");
 
+    // and remove the blue border from the input-text
+    txt.classList.remove("blue-border");
+
+    // get the background definition from the input-text...
+    let bgColor = txt.value;
+
+    // ... and set the background of the body
     document.body.style.background = bgColor;
-
 }
 
 
-function buttonFocusOut() {
-    btn.style.boxShadow = "none";
+function textClicked() {
+
+    // draw a blue border around the input-text...
+    txt.classList.add("blue-border");
+
+    // and remove the blue border from the button
+    btn.classList.remove("blue-border");
 }
